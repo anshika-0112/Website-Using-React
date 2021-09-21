0b7c7dc594d905eaa740d8b681d098e1e9cfa3c0
anshika0112 <anshikabansal13@gmail.com>
2021-09-21 09:29:20 +0530

    Made some changes

--- src/components/redux/user/userReducer.js
+++ src/components/redux/user/userReducer.js
@@ -1,12 +1,12 @@
 import { SET_USER_DETAILS } from "./userType";
 
 const initialState = {
-  userDetails: JSON.parse(sessionStorage.getItem("user"))
+  userDetails: JSON.parse(sessionStorage.getItem("user")),
 };
 
 const userReducer = (state = initialState, action) => {
   switch (action.type) {
-    case SET_USER_DETAILS:console.log(action.payload);
+    case SET_USER_DETAILS:
       return { ...state, userDetails: action.payload };
     default:
       return state;
